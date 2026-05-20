55dc5d5979db936eb3aabb5d0a01733d5dc7c2e0
khushi12321 <khushishrestha002@gmail.com>
2026-05-20 06:07:14 +0545

    Revert "Booking logic"

--- src/main/java/com/bajekocinema/services/MovieService.java
+++ src/main/java/com/bajekocinema/services/MovieService.java
@@ -11,10 +11,6 @@ import java.util.List;
 public class MovieService {
     private MovieDAO movieDAO = new MovieDAO();
     private GenreDAO genreDAO = new GenreDAO();
-    
-    public MovieModel getMovieById(int movieId) {
-        return movieDAO.getMovieById(movieId);
-    }
 
     public List<MovieModel> getAllMovies() {
         return movieDAO.getAllMovies();
